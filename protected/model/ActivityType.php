3ecdaaec3cc7c22e606d11ff53ebbeadd0b8ae08
ivan.mananjara <ivan.mananjara@gmail.com>
2014-02-05 22:11:37 +0000

    We can create, now,  an activity

--- protected/model/ActivityType.php
+++ protected/model/ActivityType.php
@@ -2,4 +2,23 @@
 Doo::loadModel('base/ActivityTypeBase');
 
 class ActivityType extends ActivityTypeBase{
+	
+	/**
+	 * Get all types of activity
+	 * return $types : Activity Type List
+	 */
+	public static function _getActivityTypes() {
+		
+		$option = array(
+				'asArray' 	=> true,
+		);
+		
+		$types = new ActivityType();
+		$types = $types->find($option);
+		
+		if(empty($types)) return null;
+		
+		return $types;
+		
+	}
 }
