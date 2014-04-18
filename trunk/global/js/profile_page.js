df28436617b5e5bdd81f8d6580c0f91c4d28960b
ivan.mananjara@gmail.com <ivan.mananjara@gmail.com@76827907-a290-d220-16ef-c2a995c769a0>
2014-04-18 21:15:30 +0000

    Notifications system verification (incomplete)

--- trunk/global/js/profile_page.js
+++ trunk/global/js/profile_page.js
@@ -66,6 +66,12 @@ $( document ).ready(function() {
 		$("#simple_alert_tl").val((parseInt($("#simple_alert_tl_day").val()) * 86400) + (parseInt($("#simple_alert_tl_hour").val()) * 3600) + (parseInt($("#simple_alert_tl_minute").val()) * 60));
 		$("#critical_alert_tl").val((parseInt($("#critical_alert_tl_day").val()) * 86400) + (parseInt($("#critical_alert_tl_hour").val()) * 3600) + (parseInt($("#critical_alert_tl_minute").val()) * 60));
 		
+		if ((parseInt($("#simple_alert_tl").val()) == 0) || (parseInt($("#critical_alert_tl").val()) == 0))
+		{
+			notification('alert-danger', '<strong>Erreur lors de la modification des notifications</strong> : Seule l\'alerte "Fin de l\'activité" peut apparaitre au moment où une activité prend fin');
+			return;
+		}
+		
 		if (parseInt($("#simple_alert_tl").val()) < parseInt($("#critical_alert_tl").val()))
 		{
 			notification('alert-danger', '<strong>Erreur lors de la modification des notifications</strong> : L\'alerte de niveau 1 doit apparaitre avant l\'alerte de niveau 2');
