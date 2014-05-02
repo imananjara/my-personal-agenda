0f93ae894d9b31a181f6ea9b514397c926b0932f
ivan.mananjara@gmail.com <ivan.mananjara@gmail.com@76827907-a290-d220-16ef-c2a995c769a0>
2014-05-02 13:11:15 +0000

    Add viewc

---
+++ trunk/protected/viewc/activities_report.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Rapport d'activites</title>
+</head>
+<body>
+<p>Bonjour</p>
+</body>
+</html>
