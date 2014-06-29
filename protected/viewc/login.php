1d10e975626865d157b77898b06a1225627cccda
ivan.mananjara <ivan.mananjara@gmail.com>
2014-06-29 13:24:45 +0000

    Change login page's design.

--- protected/viewc/login.php
+++ protected/viewc/login.php
@@ -11,88 +11,69 @@
 	<script type="text/javascript" src="<?php echo $data['globalurl']; ?>js/bootstrap-extensions/bootstrap-datepicker/bootstrap-datetimepicker.fr.js"></script>
 </head>
 <body>
-	<input type="hidden" id="base-url" value="<?php echo $data['baseurl']; ?>">
+	<input type="hidden" id="base-url" value="<?php echo $data['baseurl']; ?>"><br><br>
 	<div class="row">
-		<div class="col-md-6 col-md-offset-3 well">
+		<div class="col-md-4 col-md-offset-4">
+			<div align="center"><img src="<?php echo $data['globalurl']; ?>img/mpa_logo.gif" alt="mpa_logo"/></div><br><br>
 			<div id="notifications-login" class='alert alert-notification'></div>
 			<div id="ajax-loader"><img src="<?php echo $data['globalurl']; ?>img/ajaxloader.gif"/></div>
-			<img src="<?php echo $data['globalurl']; ?>img/mpa_logo.gif" alt="mpa_logo"/><br><br>
 			
-			<ul class="nav nav-tabs">
+			
+			<ul class="nav nav-pills nav-justified">
 			  <li class="active"><a id="connexion-tab" href="#connexion-content" data-toggle="tab">Connexion</a></li>
 			  <li><a id="inscription-tab" href="#inscription-content" data-toggle="tab">Inscription</a></li>
 			</ul>
 			
 			<br>
 			<div class="tab-content">
-				<div class="tab-pane fade in active" id="connexion-content">
-					<form id="connexion-form" class="form-horizontal" method="post" role="form">
-					  <div class="form-group">
-					    <label for="inputLoginAuth" class="col-sm-3 control-label">Pseudonyme</label>
-					    <div class="col-sm-9">
+				<div class="tab-pane fade in active well custom-well" id="connexion-content">
+					<form id="connexion-form" method="post" role="form">
+					  <div class="form-group input-group">
+					  	  <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
 					      <input type="text" class="form-control" name="inputLoginAuth" id="inputLoginAuth" placeholder="Pseudonyme">
-					    </div>
 					  </div>
-					  <div class="form-group">
-					    <label for="inputPasswordAuth" class="col-sm-3 control-label">Mot de passe</label>
-					    <div class="col-sm-9">
+					  <div class="form-group input-group">
+					  	  <span class="input-group-addon"><span class="glyphicon glyphicon-credit-card"></span></span>
 					      <input type="password" class="form-control" name="inputPasswordAuth" id="inputPasswordAuth" placeholder="Mot de passe">
-					    </div>
 					  </div>
 					  <div class="form-group">
-					    <div class="col-sm-offset-3 col-sm-10">
 					      <div class="checkbox">
 					        <label>
 					          <input type="checkbox" name="admin_page_access"> Accéder à la page d'administration *
 					        </label>
-					      </div>
 					    </div>
 					  </div>
 					  <div class="form-group">
-					    <div class="col-sm-offset-3 col-sm-9">
-					      <a href="javascript:void(0)" id="to-connect-action" class="btn btn-default">Se connecter</a>
-					    </div>
+					      <a href="javascript:void(0)" id="to-connect-action" class="btn btn-success">Se connecter</a>
 					  </div>
 					</form><br>
 					<p>* Reservé uniquement aux administrateurs de l'application</p>
 				</div>
 				
-				<div class="tab-pane fade" id="inscription-content">
-					<form id="inscription-form" class="form-horizontal" method="post" role="form">
+				<div class="tab-pane fade well custom-well" id="inscription-content">
+					<form id="inscription-form" method="post" role="form">
 					  <div class="form-group">
-					    <label for="inputLoginInscrip" class="col-sm-3 control-label">Pseudonyme</label>
-					    <div class="col-sm-9">
-					      <input type="text" class="form-control" name="inputLoginInscrip" id="inputLoginInscrip" placeholder="Pseudonyme">
-					    </div>
+					    <label for="inputLoginInscrip">Pseudonyme :</label>
+					    <input type="text" class="form-control" name="inputLoginInscrip" id="inputLoginInscrip" placeholder="Pseudonyme">
 					  </div>
 					  <div class="form-group">
-					    <label for="inputPasswordInscrip" class="col-sm-3 control-label">Mot de passe</label>
-					    <div class="col-sm-9">
-					      <input type="password" class="form-control" name="inputPasswordInscrip" id="inputPasswordInscrip" placeholder="Mot de passe">
-					    </div>
+					    <label for="inputPasswordInscrip">Mot de passe :</label>
+					    <input type="password" class="form-control" name="inputPasswordInscrip" id="inputPasswordInscrip" placeholder="Mot de passe">
 					  </div>
 					  <div class="form-group">
-					    <label for="inputFirstNameInscrip" class="col-sm-3 control-label">Prénom</label>
-					    <div class="col-sm-9">
-					      <input type="text" class="form-control" name="inputFirstNameInscrip" id="inputFirstNameInscrip" placeholder="Prénom">
-					    </div>
+					    <label for="inputFirstNameInscrip">Prénom :</label>
+					    <input type="text" class="form-control" name="inputFirstNameInscrip" id="inputFirstNameInscrip" placeholder="Prénom">
 					  </div>
 					  <div class="form-group">
-					    <label for="inputLastNameInscrip" class="col-sm-3 control-label">Nom</label>
-					    <div class="col-sm-9">
-					      <input type="text" class="form-control" name="inputLastNameInscrip" id="inputLastNameInscrip" placeholder="Nom">
-					    </div>
+					    <label for="inputLastNameInscrip">Nom :</label>
+					    <input type="text" class="form-control" name="inputLastNameInscrip" id="inputLastNameInscrip" placeholder="Nom">
 					  </div>  
 					  <div class="form-group">
-					    <label for="inputEmailInscrip" class="col-sm-3 control-label">Email</label>
-					    <div class="col-sm-9">
-					      <input type="text" class="form-control" name="inputEmailInscrip" id="inputEmailInscrip" placeholder="Email">
-					    </div>
+					    <label for="inputEmailInscrip">Email :</label>
+					    <input type="text" class="form-control" name="inputEmailInscrip" id="inputEmailInscrip" placeholder="Email">
 					  </div>
 					  <div class="form-group">
-					    <div class="col-sm-offset-3 col-sm-9">
-					      <a href="javascript:void(0)" id="to-subscribe-action" class="btn btn-default">S'inscrire</a>
-					   </div>
+					    <a href="javascript:void(0)" id="to-subscribe-action" class="btn btn-success">S'inscrire</a>
 					  </div>
 					</form>
 				</div>
