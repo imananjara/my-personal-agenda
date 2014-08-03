a842fcea19a6091e6b3d2adb5add425a0f01e325
ivan.mananjara@gmail.com <ivan.mananjara@gmail.com@76827907-a290-d220-16ef-c2a995c769a0>
2014-08-03 21:11:12 +0000

    Update (design) error's page.

--- trunk/protected/viewc/error.php
+++ trunk/protected/viewc/error.php
@@ -9,7 +9,7 @@
 <body>
 	<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//menu_bar.php"; ?>
 	<div class="row">
-		<div class="col-md-6 col-md-offset-3 well">
+		<div class="col-md-6 col-md-offset-3 well custom-well">
 			<div align="center"><img src="<?php echo $data['globalurl']; ?>/img/mpa_logo.gif"/></div>
 			<h3 align="center">Pas de chance, la page que vous cherchez est inexistante !!!</h3><br>
 			<p align="center">Pour revenir sur la page principale, vous pouvez cliquer sur le bouton ci-dessous.</p><br>
