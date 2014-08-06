692d640b98e29af194dcc02e082fbcc4a521b7c4
ivan.mananjara@gmail.com <ivan.mananjara@gmail.com@76827907-a290-d220-16ef-c2a995c769a0>
2014-08-06 21:43:34 +0000

    - Summernote is added to the activity edition's page. - New design for the activity's page (Not finished yet)

--- trunk/protected/viewc/activity.php
+++ trunk/protected/viewc/activity.php
@@ -9,122 +9,127 @@
 	<?php endif; ?>
 	<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//css_files.php"; ?>
 	<link rel="stylesheet" href="<?php echo $data['globalurl']; ?>css/bootstrap-extensions/bootstrap-datepicker/bootstrap-datetimepicker.min.css">
+	<link href="<?php echo $data['globalurl']; ?>css/bootstrap-extensions/summernote/summernote.css" rel="stylesheet">
 	<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//js_files.php"; ?>
 	<script type="text/javascript" src="<?php echo $data['globalurl']; ?>js/bootstrap-extensions/bootstrap-datepicker/moment.min.js"></script>
 	<script type="text/javascript" src="<?php echo $data['globalurl']; ?>js/bootstrap-extensions/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
 	<script type="text/javascript" src="<?php echo $data['globalurl']; ?>js/bootstrap-extensions/bootstrap-datepicker/bootstrap-datetimepicker.fr.js"></script>
+	<script type="text/javascript" src="<?php echo $data['globalurl']; ?>js/bootstrap-extensions/summernote/summernote.min.js"></script>
+	<script type="text/javascript" src="<?php echo $data['globalurl']; ?>js/bootstrap-extensions/summernote/lang/summernote-fr-FR.js"></script>
 </head>
 <body>
 	<div id="notifications" class='alert alert-notification'></div>
 	<?php include Doo::conf()->SITE_PATH .  Doo::conf()->PROTECTED_FOLDER . "viewc//menu_bar.php"; ?>
 	<div class="row">
-  		<div class="col-md-6 col-md-offset-3 well">
+  		<div class="col-md-10 col-md-offset-1">
+  			<div class="well custom-well">
   			<?php if( isset($data['activity']) ): ?>
- 			<div>EDITION DE L'ACTIVITE <strong>"<?php echo $data['activity']['title']; ?>"</strong></div>
+ 			<div class="custom-well-header">Edition de l'activité <strong>"<?php echo $data['activity']['title']; ?>"</strong></div>
  			<?php else: ?>
- 			<div>CREATION D'UNE ACTIVITE</div>
+ 			<div class="custom-well-header">Création d'une activité</div>
  			<?php endif; ?>
- 			<hr>
- 			<form id="save-activity-form" method="post" action="<?php echo $data['baseurl']; ?>saveactivity" class="form-horizontal" role="form">
- 			  <?php if( isset($data['activity']) ): ?>
- 			  <input type="hidden" name="idActivity" id="idActivity" value="<?php echo $data['activity']['activity_id']; ?>"/>
- 			  <?php endif; ?>
-			  <div class="form-group">
-			    <label for="activityTitle" class="col-sm-2 control-label">Titre</label>
-			    <div class="col-sm-5">
-			      <?php if( isset($data['activity']) ): ?>
-			      <input type="text" class="form-control" value="<?php echo $data['activity']['title']; ?>" name="activityTitle" id="activityTitle" placeholder="Titre de l'activite"/>
-			      <?php else: ?>
-			      <input type="text" class="form-control" name="activityTitle" id="activityTitle" placeholder="Titre de l'activite"/>
-			      <?php endif; ?>
-			    </div>
-			  </div>
-			  <?php if( isset($data['activitytypes']) ): ?>
-			  <div class="form-group">
-			    <label for="activityTypes" class="col-sm-2 control-label">Type</label>
-			    <div class="col-sm-10">
-			      <select id="activityTypes" name="activityTypes" class="form-control">
-			      	  <?php foreach($data['activitytypes'] as $k1=>$v1): ?>
-			      	  <?php if( isset($data['activity']) && $v1['activity_type_id'] == $data['activity']['activity_type_id'] ): ?>
-			      	  <option selected="selected" value="<?php echo $v1['activity_type_id']; ?>"><?php echo $v1['activity_type_name']; ?></option>
-			      	  <?php else: ?>
-			      	  <option value="<?php echo $v1['activity_type_id']; ?>"><?php echo $v1['activity_type_name']; ?></option>
-			      	  <?php endif; ?>
-			      	  <?php endforeach; ?>
-				  </select>
-			    </div>
-			  </div>
-			  <?php endif; ?>
-			  <div class="form-group">
-			    <label for="activityDescription" class="col-sm-2 control-label">Description</label>
-			    <div class="col-sm-10">
-			      <?php if( isset($data['activity']) ): ?>
-			      <input type="text" class="form-control" value="<?php echo $data['activity']['description']; ?>" name="activityDescription" id="activityDescription" placeholder="Description de l'activite"/>
-			      <?php else: ?>
-			      <input type="text" class="form-control" name="activityDescription" id="activityDescription" placeholder="Description de l'activite"/>
-			      <?php endif; ?>
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <label for="activityEndDate" class="col-sm-2 control-label">Echéance</label>
-			    <div class="col-sm-5">
-			      <div class='input-group date' id="activityEndDate">
-			      <?php if( isset($data['activity']) ): ?>
-			      <input type="text" class="form-control" value="<?php echo $data['activity']['end_date']; ?>" placeholder="Date de fin de l'activite" name="activityEndDate">
-			      <?php else: ?>
-			      <input type="text" class="form-control" placeholder="Date de fin de l'activite" name="activityEndDate">
-			      <?php endif; ?>
-			      <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
-			      </div>
-			    </div>
-			    <div class="col-sm-5">
-			    	<div class='input-group date' id="activityEndHour">
-			    	<?php if( isset($data['activity']) ): ?>
-			    	<input type="text" class="form-control" value="<?php echo $data['activity']['end_hour']; ?>" placeholder="Heure de fin de l'activite" name="activityEndHour"/>
-			    	<?php else: ?>
-			    	<input type="text" class="form-control" placeholder="Heure de fin de l'activite" name="activityEndHour"/>
-			    	<?php endif; ?>
-			    	<span class="input-group-addon"><span class="glyphicon glyphicon-time"></span></span>
-			    	</div>
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <label for="activityDone" class="col-sm-2 control-label">Fait à (en %)</label>
-			    <div class="col-sm-10">
-			      <select class="form-control" name="activityDone" id="activityDone">
-					  <?php foreach(range(0, 100, 10) as $data['i']): ?>
-					  <?php if( isset($data['activity']) && $data['i'] == $data['activity']['percent_done'] ): ?>
-					  <option selected="selected" value="<?php echo $data['i']; ?>"><?php echo $data['i']; ?></option>
-					  <?php else: ?>
-					  <option value="<?php echo $data['i']; ?>"><?php echo $data['i']; ?></option>
-					  <?php endif; ?>
-					  <?php endforeach; ?>
-				  </select>
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <label for="activityCommentary" class="col-sm-2 control-label">Commentaire</label>
-			    <div class="col-sm-10">
-			      <?php if( isset($data['activity']) ): ?>
-			      <textarea id="activityCommentary" name="activityCommentary" placeholder="Commentaires a saisir ici..." class="form-control" rows="5"><?php echo $data['activity']['commentary']; ?></textarea>
-			      <?php else: ?>
-			      <textarea id="activityCommentary" name="activityCommentary" placeholder="Commentaires a saisir ici..." class="form-control" rows="5"></textarea>
-			      <?php endif; ?>
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <div class="col-sm-offset-2 col-sm-10">
-			      <?php if( isset($data['activity']) ): ?>
-			      <a id="save-activity-btn" class="btn btn-default">Editer l'activité</a>
-			      <?php else: ?>
-			      <a id="save-activity-btn" class="btn btn-default">Créer l'activité</a>
-			      <?php endif; ?>
-			    </div>
-			  </div>
-			</form>
-  		
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<div class="row">
+ 		<div class="col-md-5 col-md-offset-1">
+	 		<div class="well custom-well">
+	 			<div class="custom-well-header">Informations principales</div><hr>
+	 			<form id="save-activity-form" method="post" action="<?php echo $data['baseurl']; ?>saveactivity">
+	 			  <?php if( isset($data['activity']) ): ?>
+	 			  <input type="hidden" name="idActivity" id="idActivity" value="<?php echo $data['activity']['activity_id']; ?>"/>
+	 			  <?php endif; ?>
+				  <div class="form-group">
+				    <label for="activityTitle" class="control-label">Titre :</label>
+				    <?php if( isset($data['activity']) ): ?>
+				    <input type="text" class="form-control" value="<?php echo $data['activity']['title']; ?>" name="activityTitle" id="activityTitle" placeholder="Titre de l'activite"/>
+				    <?php else: ?>
+				    <input type="text" class="form-control" name="activityTitle" id="activityTitle" placeholder="Titre de l'activite"/>
+				    <?php endif; ?>
+				  </div>
+				  <?php if( isset($data['activitytypes']) ): ?>
+				  <div class="form-group">
+				    <label for="activityTypes" class="control-label">Type :</label>
+				      <select id="activityTypes" name="activityTypes" class="form-control">
+				      	  <?php foreach($data['activitytypes'] as $k1=>$v1): ?>
+				      	  <?php if( isset($data['activity']) && $v1['activity_type_id'] == $data['activity']['activity_type_id'] ): ?>
+				      	  <option selected="selected" value="<?php echo $v1['activity_type_id']; ?>"><?php echo $v1['activity_type_name']; ?></option>
+				      	  <?php else: ?>
+				      	  <option value="<?php echo $v1['activity_type_id']; ?>"><?php echo $v1['activity_type_name']; ?></option>
+				      	  <?php endif; ?>
+				      	  <?php endforeach; ?>
+					  </select>
+				  </div>
+				  <?php endif; ?>
+				  <div class="form-group">
+				    <label for="activityDescription" class="control-label">Description :</label>
+				    <?php if( isset($data['activity']) ): ?>
+				    <input type="text" class="form-control" value="<?php echo $data['activity']['description']; ?>" name="activityDescription" id="activityDescription" placeholder="Description de l'activite"/>
+				    <?php else: ?>
+				    <input type="text" class="form-control" name="activityDescription" id="activityDescription" placeholder="Description de l'activite"/>
+				    <?php endif; ?>
+				  </div>
+				  <div class="form-group">
+				    <label for="activityEndDate" class="control-label">Echéance :</label>
+				    <div class="row">
+					    <div class="col-sm-6">
+					      <div class='input-group date' id="activityEndDate">
+					      <?php if( isset($data['activity']) ): ?>
+					      <input type="text" class="form-control" value="<?php echo $data['activity']['end_date']; ?>" placeholder="Date de fin de l'activite" name="activityEndDate">
+					      <?php else: ?>
+					      <input type="text" class="form-control" placeholder="Date de fin de l'activite" name="activityEndDate">
+					      <?php endif; ?>
+					      <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+					      </div>
+					    </div>
+					    <div class="col-sm-6">
+					    	<div class='input-group date' id="activityEndHour">
+					    	<?php if( isset($data['activity']) ): ?>
+					    	<input type="text" class="form-control" value="<?php echo $data['activity']['end_hour']; ?>" placeholder="Heure de fin de l'activite" name="activityEndHour"/>
+					    	<?php else: ?>
+					    	<input type="text" class="form-control" placeholder="Heure de fin de l'activite" name="activityEndHour"/>
+					    	<?php endif; ?>
+					    	<span class="input-group-addon"><span class="glyphicon glyphicon-time"></span></span>
+					    	</div>
+					    </div>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="activityDone" class="control-label">Fait à (en %)</label>
+				      <select class="form-control" name="activityDone" id="activityDone">
+						  <?php foreach(range(0, 100, 10) as $data['i']): ?>
+						  <?php if( isset($data['activity']) && $data['i'] == $data['activity']['percent_done'] ): ?>
+						  <option selected="selected" value="<?php echo $data['i']; ?>"><?php echo $data['i']; ?></option>
+						  <?php else: ?>
+						  <option value="<?php echo $data['i']; ?>"><?php echo $data['i']; ?></option>
+						  <?php endif; ?>
+						  <?php endforeach; ?>
+					  </select>
+				  </div>
+				  <div class="form-group">
+				    <label for="activityCommentary" class="control-label">Commentaire :</label><br>
+				      <?php if( isset($data['activity']) ): ?>
+				      <textarea id="activityCommentary" name="activityCommentary" placeholder="Commentaires a saisir ici..." class="form-control" rows="5"><?php echo $data['activity']['commentary']; ?></textarea>
+				      <?php else: ?>
+				      <textarea id="activityCommentary" name="activityCommentary" placeholder="Commentaires a saisir ici..." class="form-control" rows="5"></textarea>
+				      <?php endif; ?>
+				  </div>
+				  <div class="form-group">
+				      <?php if( isset($data['activity']) ): ?>
+				      <a id="save-activity-btn" class="btn btn-success">Editer l'activité</a>
+				      <?php else: ?>
+				      <a id="save-activity-btn" class="btn btn-success">Créer l'activité</a>
+				      <?php endif; ?>
+				  </div>
+				</form>
+  			</div>
   		</div>
-	</div>
+  		<div class="col-md-5">
+	  		<div class="well custom-well">
+	  			<div class="custom-well-header">Liste des tâches</div><hr>
+	  		</div>
+  		</div>
+  	</div>
 </body>
 <script type="text/javascript" src='<?php echo $data['globalurl']; ?>js/activity_page.js'></script>
 </html>
