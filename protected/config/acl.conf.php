958a5b55efd37b34bb2df0de462df954ecb8fc5d
ivan.mananjara <ivan.mananjara@gmail.com>
2015-03-13 16:23:15 +0000

    Install Laravel 5

--- protected/config/acl.conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$acl['auth']['allow'] = array('*');
-
-// anonymous users can only access to the methods of AuthenticationController
-$acl['anonymous']['allow'] = array(
-		'AuthenticationController'=> array('getLoginPage', 'adduser', 'getSession', 'toLogout')
-);
-?>
